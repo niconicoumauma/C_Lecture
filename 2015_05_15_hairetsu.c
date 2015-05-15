9934e3fd5d9273c893057be34ff09a8a53e8c591
Yamanaka Taro <isaiahhk2362@gmail.com>
2015-05-15 19:00:19 +0900

    Update 2015_05_15_hairetsu.c

--- 2015_05_15_hairetsu.c
+++ 2015_05_15_hairetsu.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(void){
-	int i, a[5], sum = 0;
-	for(i=0; i<5; i++){
-		printf(“a[%d] : ”, i);
-		scanf(“%d”, &a[i]);
-	}
-	for(i=0; i<5; i++){
-		//ここに記述せよ
-	}
-	printf(“average : %f\n”, (double)sum/5);
+        int i, a[5], sum = 0;
+        for(i=0; i<5; i++){
+                printf("a[%d] : ", i);
+                scanf("%d", &a[i]);
+        }
+        for(i=0; i<5; i++){
+                sum += a[i];
+        }
+        printf("average : %f\n", (double)sum/5);
 }
