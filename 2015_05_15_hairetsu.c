97daca8f51e889d117e8af96175ed57714f4c816
Yamanaka Taro <isaiahhk2362@gmail.com>
2015-05-15 19:11:48 +0900

    Update 2015_05_15_hairetsu.c

--- 2015_05_15_hairetsu.c
+++ 2015_05_15_hairetsu.c
@@ -3,7 +3,7 @@ int main(void){
         int i, a[5], sum = 0;
         for(i=0; i<5; i++){
                 printf("a[%d] : ", i);
-                scanf("%d", &a[i]);
+                // edit this line
         }
         for(i=0; i<5; i++){
                 // edit this line
